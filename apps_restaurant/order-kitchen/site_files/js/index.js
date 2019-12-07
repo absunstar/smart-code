1509bb1d3faf3e6acd6b86b7b501a09cf29975b3
absunstar <absunstar@gmail.com>
2019-12-07 16:49:52 +0200

    #

--- apps_restaurant/order-kitchen/site_files/js/index.js
+++ apps_restaurant/order-kitchen/site_files/js/index.js
@@ -101,6 +101,7 @@ app.controller("order_kitchen", function ($scope, $http, $interval) {
   };
 
   $scope.orderKitchensList = function () {
+    $scope.error = "";
     $scope.busy = true;
     $http({
       method: "POST",
