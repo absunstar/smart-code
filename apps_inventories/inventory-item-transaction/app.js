27311f1dae1d02cf6e07aec7af3f109d2bd246af
absunstar <absunstar@gmail.com>
2019-12-18 16:57:25 +0200

    #

--- apps_inventories/inventory-item-transaction/app.js
+++ apps_inventories/inventory-item-transaction/app.js
@@ -104,7 +104,9 @@ module.exports = function init(site) {
     delete itm.id
     delete itm._id
 
-    $item_transaction.findMany({ sort: { id: -1 }, where: { size: itm.size, 'barcode': itm.barcode, name: itm.name, 'branch.code': itm.branch.code, 'company.id': itm.company.id }, limit: 1 }, (err, docs) => {
+    console.log(itm)
+
+    $item_transaction.findMany({ sort: { id: -1 }, where: { size: itm.size, 'barcode': itm.barcode, name: itm.name, 'branch.code': itm.branch.code, 'company.id': itm.company.id , 'store.id' : itm.store.id }, limit: 1 }, (err, docs) => {
 
       if (docs && docs.length === 1) {
         itm.last_count = docs[0].current_count
