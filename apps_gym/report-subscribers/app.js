dd932deece4ad52516f1a412d066ad539a97958a
AbduAllah Gabbar <you@example.com>
2019-12-24 22:53:39 +0200

    edit

---
+++ apps_gym/report-subscribers/app.js
@@ -0,0 +1,111 @@
+module.exports = function init(site) {
+  const $request_service = site.connectCollection("request_service")
+  const $account_invoices = site.connectCollection("account_invoices")
+
+  site.get({
+    name: "report_subscribers",
+    path: __dirname + "/site_files/html/index.html",
+    parser: "html",
+    compress: true
+  })
+
+  site.get({
+    name: 'images',
+    path: __dirname + '/site_files/images/'
+  })
+
+
+  site.post("/api/report_subscribers/all", (req, res) => {
+    let response = {
+      done: false
+    }
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    let where = req.data.where || {}
+
+    if (where['code']) {
+      where['code'] = new RegExp(where['code'], 'i')
+    }
+
+    if (where['name']) {
+      where['name'] = new RegExp(where['name'], 'i')
+    }
+    if (where.date) {
+      let d1 = site.toDate(where.date)
+      let d2 = site.toDate(where.date)
+      d2.setDate(d2.getDate() + 1)
+      where.date = {
+        '$gte': d1,
+        '$lt': d2
+      }
+    } else if (where && where.date_from) {
+      let d1 = site.toDate(where.date_from)
+      let d2 = site.toDate(where.date_to)
+      d2.setDate(d2.getDate() + 1);
+      where.date = {
+        '$gte': d1,
+        '$lt': d2
+      }
+      delete where.date_from
+      delete where.date_to
+    }
+
+    if (where['shift_code']) {
+      where['shift.code'] = new RegExp(where['shift_code'], 'i')
+      delete where['shift_code']
+    }
+
+    if (where['order_subscribers_type']) {
+      where['order_subscribers_type.id'] = where['order_subscribers_type'].id;
+      delete where['order_subscribers_type']
+    }
+
+    if (where['customer']) {
+      where['customer.id'] = where['customer'].id;
+      delete where['customer']
+    }
+
+    where['company.id'] = site.get_company(req).id
+    where['branch.code'] = site.get_branch(req).code
+
+    $request_service.findMany({
+      select: req.body.select || {},
+      where: where,
+      sort: req.body.sort || { id: -1 },
+      limit: req.body.limit
+    }, (err, request_docs, count) => {
+
+      if (!err) {
+        response.done = true
+        $account_invoices.findMany({
+          where: { 'source_type.id': 4 }
+        }, (invoice_err, invoice_docs) => {
+          if (!invoice_err) {
+            request_docs.forEach(_request => {
+              invoice_docs.forEach(_invoice => {
+                if (_invoice.invoice_id == _request.id) {
+                  _request.total_paid_up = _invoice.total_paid_up
+                  _request.total_remain = _invoice.total_remain
+                }
+
+              });
+              if (_request.total_remain == (null || undefined))
+              _request.total_remain = _request.paid_require
+            });
+            response.list = request_docs
+            response.count = count
+          } else {
+            response.error = err.message
+          }
+          res.json(response)
+        })
+      }
+    })
+  })
+
+}
---
+++ apps_gym/report-subscribers/site_files/js/index.js
@@ -0,0 +1,123 @@
+app.controller("report_subscribers", function ($scope, $http, $timeout) {
+  $scope._search = {};
+
+  $scope.report_subscribers = {};
+
+  $scope.getPaymentMethodList = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $scope.paymentMethodList = [];
+    $http({
+      method: "POST",
+      url: "/api/payment_method/all"
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        $scope.paymentMethodList = response.data;
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.getSourceType = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $scope.sourceTypeList = [];
+    $http({
+      method: "POST",
+      url: "/api/source_type/all"
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (site.feature('gym')) $scope.sourceTypeList = response.data.filter(i => i.id != 3);
+        else if (site.feature('restaurant')) $scope.sourceTypeList = response.data.filter(i => i.id != 4);
+        else if (site.feature('pos')) $scope.sourceTypeList = response.data.filter(i => i.id != 4 && i.id != 3);
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.getReportServicesList = function (where) {
+    $scope.busy = true;
+    $scope.list = [];
+    $scope.count = 0;
+    $http({
+      method: "POST",
+      url: "/api/report_subscribers/all",
+      data: {
+        where: where
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done && response.data.list.length > 0) {
+          $scope.list = response.data.list;
+          $scope.count = response.data.count;
+          $scope.paid_require = 0;
+          $scope.total_discount = 0;
+          $scope.total_paid_up = 0;
+          $scope.total_remain = 0;
+          $scope.list.forEach(request => {
+            $scope.paid_require += request.paid_require || 0;
+            $scope.total_discount += request.total_discount || 0;
+            $scope.total_paid_up += request.total_paid_up || 0;
+            $scope.total_remain += request.total_remain || 0;
+          });
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.getCustomerList = function (ev) {
+    $scope.error = '';
+    $scope.busy = true;
+    if (ev.which === 13) {
+      $http({
+        method: "POST",
+        url: "/api/customers/all",
+        data: {
+          search: $scope.search_customer
+          /*  select: {
+            id: 1,
+            name_ar: 1,
+            name_en: 1,
+          } */
+        }
+      }).then(
+        function (response) {
+          $scope.busy = false;
+          if (response.data.done && response.data.list.length > 0) {
+            $scope.customersList = response.data.list;
+          }
+        },
+        function (err) {
+          $scope.busy = false;
+          $scope.error = err;
+        }
+      )
+    };
+  };
+
+  $scope.searchAll = function () {
+    $scope._search = {};
+
+    $scope.getReportServicesList($scope.search);
+    $scope.customer = $scope.search.customer;
+    $scope.search = {};
+    site.hideModal('#reportServicesSearchModal');
+  };
+
+  $scope.getReportServicesList({ date: new Date() });
+  $scope.getPaymentMethodList();
+  $scope.getSourceType();
+});
