30dcd5593da39140e1d1282bc80b5004eef04811
AbduAllah Gabbar <you@example.com>
2019-12-26 01:41:47 +0200

    edit

---
+++ apps_core/currency/app.js
@@ -0,0 +1,199 @@
+module.exports = function init(site) {
+  const $currency = site.connectCollection("currency")
+
+  site.get({
+    name: 'images',
+    path: __dirname + '/site_files/images/'
+  })
+
+  site.get({
+    name: "currency",
+    path: __dirname + "/site_files/html/index.html",
+    parser: "html",
+    compress: true
+  })
+
+  site.post("/api/currency/add", (req, res) => {
+    let response = {
+      done: false
+    }
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    let currency_doc = req.body
+    currency_doc.$req = req
+    currency_doc.$res = res
+
+    currency_doc.add_user_info = site.security.getUserFinger({
+      $req: req,
+      $res: res
+    })
+
+    if (typeof currency_doc.active === 'undefined') {
+      currency_doc.active = true
+    }
+
+    currency_doc.company = site.get_company(req)
+/*     currency_doc.branch = site.get_branch(req)
+ */
+    $currency.find({
+      'company.id': site.get_company(req).id,
+/*       'branch.code': site.get_branch(req).code,
+ */      where: {
+        'name': currency_doc.name
+      }
+    }, (err, doc) => {
+      if (!err && doc) {
+
+        response.error = 'Name Exists'
+        res.json(response)
+      } else {
+        $currency.add(currency_doc, (err, doc) => {
+          if (!err) {
+            response.done = true
+            response.doc = doc
+          } else {
+            response.error = err.message
+          }
+          res.json(response)
+        })
+      }
+    })
+  })
+
+  site.post("/api/currency/update", (req, res) => {
+    let response = {
+      done: false
+    }
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    let currency_doc = req.body
+
+    currency_doc.edit_user_info = site.security.getUserFinger({
+      $req: req,
+      $res: res
+    })
+
+    if (currency_doc.id) {
+      $currency.edit({
+        where: {
+          id: currency_doc.id
+        },
+        set: currency_doc,
+        $req: req,
+        $res: res
+      }, err => {
+        if (!err) {
+          response.done = true
+        } else {
+          response.error = 'Code Already Exist'
+        }
+        res.json(response)
+      })
+    } else {
+      response.error = 'no id'
+      res.json(response)
+    }
+  })
+
+  site.post("/api/currency/view", (req, res) => {
+    let response = {
+      done: false
+    }
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    $currency.findOne({
+      where: {
+        id: req.body.id
+      }
+    }, (err, doc) => {
+      if (!err) {
+        response.done = true
+        response.doc = doc
+      } else {
+        response.error = err.message
+      }
+      res.json(response)
+    })
+  })
+
+  site.post("/api/currency/delete", (req, res) => {
+    let response = {
+      done: false
+    }
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    let id = req.body.id
+
+    if (id) {
+      $currency.delete({
+        id: id,
+        $req: req,
+        $res: res
+      }, (err, result) => {
+        if (!err) {
+          response.done = true
+        } else {
+          response.error = err.message
+        }
+        res.json(response)
+      })
+    } else {
+      response.error = 'no id'
+      res.json(response)
+    }
+  })
+
+
+  site.post("/api/currency/all", (req, res) => {
+    let response = {
+      done: false
+    }
+
+    let where = req.body.where || {}
+
+    if (where['name']) {
+      where['name'] = new RegExp(where['name'], "i");
+    }
+
+    where['company.id'] = site.get_company(req).id
+/*     where['branch.code'] = site.get_branch(req).code
+ */
+    $currency.findMany({
+      select: req.body.select || {},
+      where: where,
+      sort: req.body.sort || {
+        id: -1
+      },
+      limit: req.body.limit
+    }, (err, docs, count) => {
+      if (!err) {
+        response.done = true
+        response.list = docs
+        response.count = count
+      } else {
+        response.error = err.message
+      }
+      res.json(response)
+    })
+  })
+
+}
---
+++ apps_core/currency/site_files/js/index.js
@@ -0,0 +1,186 @@
+app.controller("currency", function ($scope, $http, $timeout) {
+
+  $scope.currency = {};
+
+  $scope.displayAddCurrency = function () {
+    $scope.error = '';
+    $scope.currency = {
+      image_url: '/images/currency.png',
+      active: true
+
+    };
+    site.showModal('#currencyAddModal');
+
+  };
+
+  $scope.addCurrency = function () {
+    $scope.error = '';
+    const v = site.validated('#currencyAddModal');
+    if (!v.ok) {
+      $scope.error = v.messages[0].ar;
+      return;
+    }
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/currency/add",
+      data: $scope.currency
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#currencyAddModal');
+          $scope.getCurrencyList();
+        } else {
+          $scope.error = response.data.error;
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    )
+  };
+
+  $scope.displayUpdateCurrency = function (currency) {
+    $scope.error = '';
+    $scope.viewCurrency(currency);
+    $scope.currency = {};
+    site.showModal('#currencyUpdateModal');
+  };
+
+  $scope.updateCurrency = function () {
+    $scope.error = '';
+    const v = site.validated('#currencyUpdateModal');
+    if (!v.ok) {
+      $scope.error = v.messages[0].ar;
+      return;
+    }
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/currency/update",
+      data: $scope.currency
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#currencyUpdateModal');
+          $scope.getCurrencyList();
+        } else {
+          $scope.error = 'Please Login First';
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    )
+  };
+
+  $scope.displayDetailsCurrency = function (currency) {
+    $scope.error = '';
+    $scope.viewCurrency(currency);
+    $scope.currency = {};
+    site.showModal('#currencyViewModal');
+  };
+
+  $scope.viewCurrency = function (currency) {
+    $scope.busy = true;
+    $scope.error = '';
+    $http({
+      method: "POST",
+      url: "/api/currency/view",
+      data: {
+        id: currency.id
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.currency = response.data.doc;
+        } else {
+          $scope.error = response.data.error;
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    )
+  };
+  $scope.displayDeleteCurrency = function (currency) {
+    $scope.error = '';
+    $scope.viewCurrency(currency);
+    $scope.currency = {};
+    site.showModal('#currencyDeleteModal');
+  };
+
+  $scope.deleteCurrency = function () {
+    $scope.busy = true;
+    $scope.error = '';
+
+    $http({
+      method: "POST",
+      url: "/api/currency/delete",
+      data: {
+        id: $scope.currency.id
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#currencyDeleteModal');
+          $scope.getCurrencyList();
+        } else {
+          $scope.error = response.data.error;
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    )
+  };
+
+  $scope.getCurrencyList = function (where) {
+    $scope.busy = true;
+    $scope.list = [];
+    $http({
+      method: "POST",
+      url: "/api/currency/all",
+      data: {
+        where: where
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done && response.data.list.length > 0) {
+          $scope.list = response.data.list;
+          $scope.count = response.data.count;
+          site.hideModal('#currencySearchModal');
+          $scope.search = {};
+
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+
+    )
+
+  };
+
+  $scope.displaySearchModal = function () {
+    $scope.error = '';
+    site.showModal('#currencySearchModal');
+
+  };
+
+  $scope.searchAll = function () {
+ 
+    $scope.getCurrencyList($scope.search);
+    site.hideModal('#currencySearchModal');
+    $scope.search = {};
+  };
+
+  $scope.getCurrencyList();
+
+});
