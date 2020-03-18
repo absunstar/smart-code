1d283553b371798bdb4b36c819d76e2a4b1f1053
unknown <abdogabbarabdo@gmail.com>
2020-03-19 00:46:28 +0200

    edit

--- apps_inventories/inventory-stores-in/site_files/js/index.js
+++ apps_inventories/inventory-stores-in/site_files/js/index.js
@@ -334,7 +334,7 @@ app.controller("stores_in", function ($scope, $http, $timeout) {
       if (obj.items)
         obj.items.map(itm => obj.total_value += site.toNumber(itm.total));
 
-      if (obj.type.id !== 4) {
+      if (obj.type && obj.type.id !== 4) {
         obj.total_tax = 0;
         obj.total_discount = 0;
       };
