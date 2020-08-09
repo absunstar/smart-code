885bfe7ba07d9de10c66ec102b9e2813f5a449dd
AbduAllah Gabbar <you@example.com>
2020-08-09 19:42:49 +0200

    edit

---
+++ apps_reports/report-stores-balance/app.js
@@ -0,0 +1,136 @@
+module.exports = function init(site) {
+  const $stores_items = site.connectCollection("stores_items")
+
+  site.get({
+    name: "report_stores_balance",
+    path: __dirname + "/site_files/html/index.html",
+    parser: "html",
+    compress: true
+  })
+
+  site.get({
+    name: 'images',
+    path: __dirname + '/site_files/images/'
+  })
+
+  site.post("/api/report_stores_balance/all", (req, res) => {
+    let response = {
+      done: false
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    };
+
+    let where = req.data.where || {};
+
+
+    let store_id = 0;
+    let branch_code = '';
+    let unit_id = 0;
+
+    if (where.store) store_id = where.store.id
+    if (where.branch) branch_code = where.branch.code
+    if (where.unit) unit_id = where.unit.id
+
+    where['company.id'] = site.get_company(req).id
+
+    if (where && where['name']) {
+      where['name'] = new RegExp(where['name'], 'i')
+    }
+
+    if (where && where['size']) {
+      where['sizes.size'] = new RegExp(where['size'], 'i')
+      delete where['size']
+    }
+
+    if (where && where['size_en']) {
+      where['sizes.size_en'] = new RegExp(where['size_en'], 'i')
+      delete where['size_en']
+    }
+
+    if (where && where['barcode']) {
+      where['sizes.barcode'] = new RegExp(where['barcode'], 'i')
+      delete where['barcode']
+    }
+
+    if (where['branch'] && where['branch'].code) {
+      where['sizes.branches_list.code'] = where['branch'].code
+      delete where['branch']
+    }
+
+    if (where['store'] && where['store'].id) {
+      where['sizes.branches_list.stores_list.store.id'] = where['store'].id
+      delete where['store']
+    }
+
+    delete where['unit']
+
+    if (where['item_group'] && where['item_group'].id) {
+      where['item_group.id'] = where['item_group'].id
+      delete where['item_group']
+    }
+
+    $stores_items.findMany({
+      select: req.body.select || {},
+      where: where,
+      sort: req.body.sort || { id: -1 },
+      limit: req.body.limit
+    }, (err1, docs) => {
+      if (!err1) {
+
+        let i_store_list = [];
+
+
+        docs.forEach(_doc => {
+          _doc.sizes.forEach(_sizes => {
+            if (_sizes.branches_list && _sizes.branches_list.length > 0) {
+
+              _sizes.branches_list.forEach(_branch => {
+
+                if (_branch.code == branch_code) {
+                  _branch.stores_list.forEach(_store => {
+                    if (_store.store.id == store_id) {
+
+                      if (_store.size_units_list && _store.size_units_list.length > 0) {
+
+                        _store.size_units_list.forEach(_unit => {
+                          if(_unit.id == unit_id){
+
+                            i_store_list.push({
+                              name: _doc.name,
+                              item_group: _doc.item_group,
+                              size: _sizes.size,
+                              average_cost: _sizes.average_cost,
+                              size_en: _sizes.size_en,
+                              barcode: _sizes.barcode,
+                              count : _unit.current_count
+                            })
+
+                          }
+                        });
+
+                      }
+                    }
+                  });
+                }
+              });
+
+            }
+          })
+        })
+
+        response.done = true
+        response.doc = i_store_list
+        res.json(response)
+
+      } else {
+        response.error = err.message
+      }
+    })
+
+  })
+
+}
---
+++ apps_reports/report-stores-balance/site_files/js/index.js
@@ -0,0 +1,317 @@
+app.controller("report_stores_balance", function ($scope, $http, $timeout) {
+  $scope._search = {};
+
+  $scope.report_stores_balance = {};
+
+  $scope.getReportStoreBalanceList = function (where) {
+    $scope.busy = true;
+    $scope.list = [];
+
+    $http({
+      method: "POST",
+      url: "/api/report_stores_balance/all",
+      data: {
+        where: where
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+
+        if (response.data.done) {
+          $scope.count = response.data.doc.length;
+          $scope.list = response.data.doc;
+          /*   $scope.total_average_cost = 0;
+            $scope.list.forEach(_list => {
+              if(_list.store_units_list && _list.store_units_list.length > 0)
+              _list.store_units_list.map(_store_unit => $scope.total_average_cost += _store_unit.total_average_cost)
+            }); */
+
+          $scope.total_average_cost = site.toNumber($scope.total_average_cost)
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.printAccountInvoive = function (_itemsList) {
+    $scope.error = '';
+    if ($scope.busy) return;
+    $scope.busy = true;
+
+    let ip = '127.0.0.1';
+    let port = '11111';
+
+    let InvoiceDate = new Date();
+
+    if ($scope.defaultSettings.printer_program) {
+      ip = $scope.defaultSettings.printer_program.ip || '127.0.0.1';
+      port = $scope.defaultSettings.printer_program.port || '11111';
+    };
+
+    let obj_print = { data: [] };
+
+    if ($scope.defaultSettings.printer_program && $scope.defaultSettings.printer_program.printer_path)
+      obj_print.printer = $scope.defaultSettings.printer_program.printer_path.ip.trim();
+
+    if ($scope.defaultSettings.printer_program && $scope.defaultSettings.printer_program.invoice_header)
+      obj_print.data.push({
+        type: 'header',
+        value: $scope.defaultSettings.printer_program.invoice_header
+      });
+
+
+    obj_print.data.push(
+      {
+        type: 'title',
+        value: 'Total StoreBalance Items'
+      }, {
+      type: 'space'
+    }, {
+      type: 'text2',
+      value: 'Date',
+      value2: site.toDateXF(InvoiceDate)
+    }, {
+      type: 'line'
+    }, {
+      type: 'text2',
+      value: 'Total Selling Count',
+      value2: $scope.count
+    }, {
+      type: 'text2',
+      value: 'Total Selling Price',
+      value2: $scope.total
+    }, {
+      type: 'line'
+    }, {
+      type: 'text3b',
+      value: 'Item',
+      value2: " Total Price",
+      value3: 'Count'
+    }, {
+      type: 'text3b',
+      value: 'الصنف',
+      value2: "إجمالي السعر",
+      value3: 'العدد'
+    }, {
+      type: 'space'
+    });
+
+
+    _itemsList.forEach(_item => {
+      _item.total = site.toNumber(_item.total);
+
+      obj_print.data.push(
+        {
+          type: 'text3',
+          value: _item.size,
+          value2: _item.total,
+          value3: _item.count
+        }, { type: 'line' });
+
+    });
+
+    if ($scope.defaultSettings.printer_program && $scope.defaultSettings.printer_program.invoice_footer)
+      obj_print.data.push({
+        type: 'space'
+      }, {
+        type: 'footer',
+        value: $scope.defaultSettings.printer_program.invoice_footer
+      });
+
+    $http({
+      method: "POST",
+      url: `http://${ip}:${port}/print`,
+      data: obj_print
+    }).then(
+      function (response) {
+        if (response)
+          $scope.busy = false;
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+
+  };
+
+
+  $scope.loadUnits = function () {
+    $scope.busy = true;
+    $scope.unitsList = [];
+    $http({
+      method: "POST",
+      url: "/api/units/all",
+      data: {
+        select: {
+          id: 1,
+          name: 1
+        }
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.unitsList = response.data.list;
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.getDefaultSettings = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/default_setting/get",
+      data: {}
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done && response.data.doc) {
+          $scope.defaultSettings = response.data.doc;
+
+        };
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+
+  };
+
+  $scope.loadItemsGroups = function () {
+    $scope.busy = true;
+    $scope.itemsGroupList = [];
+    $http({
+      method: "POST",
+      url: "/api/items_group/all",
+      data: {
+        select: {
+          id: 1,
+          name: 1
+        }
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.itemsGroupList = response.data.list;
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.loadBranches = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/branches/all"
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.branchesList = response.data.list;
+          $scope.branchCode = response.data.branch;
+        }
+
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.loadStores = function (branch) {
+    $scope.error = '';
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/stores/all",
+      data: {
+        select: { id: 1, name: 1, type: 1 },
+        branchTo: branch
+      }
+
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.storesList = response.data.list;
+        }
+
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.loadUnits = function () {
+    $scope.busy = true;
+    $scope.unitsList = [];
+    $http({
+      method: "POST",
+      url: "/api/units/all",
+      data: {
+        select: {
+          id: 1,
+          name: 1,
+          barcode: 1
+        }
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.unitsList = response.data.list;
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.searchAll = function () {
+
+    const v = site.validated('#reportStoreBalanceSearchModal');
+    if (!v.ok) {
+      $scope.error = v.messages[0].ar;
+      return;
+    }
+
+    $scope._search = {};
+    $scope.getReportStoreBalanceList($scope.search);
+    site.hideModal('#reportStoreBalanceSearchModal');
+    $scope.search = {
+      branch: $scope.search.branch,
+      store: $scope.search.store,
+      unit: $scope.search.unit
+    }
+  };
+
+
+
+
+  $scope.getDefaultSettings();
+  $scope.loadBranches();
+  $scope.loadUnits();
+  $scope.loadItemsGroups();
+  $scope.loadUnits();
+});
