f7097869f35421313b4def07519f4f6424187899
AbduAllah Gabbar <you@example.com>
2020-10-20 00:40:17 +0200

    edit

--- apps_accounting/accounting-invoices/app.js
+++ apps_accounting/accounting-invoices/app.js
@@ -1147,7 +1147,6 @@ module.exports = function init(site) {
 
     where['company.id'] = site.get_company(req).id
     where['branch.code'] = site.get_branch(req).code
-    console.log(where);
 
     $account_invoices.findMany({
       select: req.body.select || {},
