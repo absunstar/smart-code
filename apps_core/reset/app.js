001621e379266b094f6217cd3e443c231f1640ff
AbduAllah Gabbar <you@example.com>
2020-11-09 21:12:42 +0200

    edit

--- apps_core/reset/app.js
+++ apps_core/reset/app.js
@@ -29,22 +29,20 @@ module.exports = function init(site) {
       return
     }
 
-    let whereStoreIn = {}
+    let whereStoreOut = {}
 
-    whereStoreIn['company.id'] = site.get_company(req).id
+    whereStoreOut['company.id'] = site.get_company(req).id
 
-    whereStoreIn['$or'] = [{ 'type.id': 3 }, { 'type.id': 4 }, { 'type.id': 6 }]
+    whereStoreOut['$or'] = [{ 'type.id': 3 }, { 'type.id': 4 }, { 'type.id': 6 }]
 
     $stores_out.findMany({
       select: req.body.select || {},
-      where: whereStoreIn,
+      where: whereStoreOut,
       sort: req.body.sort || {
         id: -1
       },
-    }, (err, docsStoreIn) => {
+    }, (err, docsStoreOut) => {
       if (!err) {
-
-
 
         let whereAccountInvoices = {}
 
@@ -60,14 +58,53 @@ module.exports = function init(site) {
         }, (err, docsAccountInvoices) => {
           if (!err) {
 
+            if (docsAccountInvoices && docsAccountInvoices.length > 0) {
+              if (docsStoreOut && docsStoreOut.length > 0) {
 
+                docsAccountInvoices.forEach(_d_a_i => {
+                  let foundAi = false
 
+                  docsStoreOut.forEach(_d_s_out => {
 
-            console.log(docsAccountInvoices.length, "Ssssssssssssssssssssssssss");
-            console.log(docsStoreIn.length, "wwwwwwwwwwwwwwwwwwwwww");
+                    if (_d_s_out.id === _d_a_i.invoice_id) {
+                      foundAi = true
+                      _d_a_i.total_discount = _d_s_out.total_discount
+                      _d_a_i.total_tax = _d_s_out.total_tax
+                      _d_a_i.current_book_list = _d_s_out.current_book_list
+                      _d_a_i.net_value = _d_s_out.net_value
+                      _d_a_i.total_value_added = _d_s_out.total_value_added
 
+                      if (_d_s_out.currency && _d_s_out.currency.id) {
+                        _d_a_i.currency = _d_s_out.currency
 
+                      }
 
+                      if (_d_s_out.payment_method && _d_s_out.payment_method.id) {
+                        _d_a_i.payment_method = _d_s_out.payment_method
+
+                      }
+
+                      if (_d_s_out.safe && _d_s_out.safe.id) {
+                        _d_a_i.safe = _d_s_out.safe
+
+                      }
+
+                      if (_d_s_out.paid_up) {
+                        _d_a_i.paid_up = _d_s_out.paid_up
+
+                      }
+
+                      $account_invoices.update(_d_a_i);
+                    }
+                  });
+
+                  if (!foundAi) {
+                    $account_invoices.delete(_d_a_i);
+                  }
+
+                });
+              }
+            }
 
           }
         })
@@ -82,6 +119,100 @@ module.exports = function init(site) {
 
 
 
+  site.post("/api/stores_in/handel_invoices", (req, res) => {
+    let response = {}
+    response.done = false
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    }
+
+    let whereStoreOut = {}
+
+    whereStoreOut['company.id'] = site.get_company(req).id
+
+    whereStoreOut['$or'] = [{ 'type.id': 1 }, { 'type.id': 4 }]
+
+    $stores_in.findMany({
+      select: req.body.select || {},
+      where: whereStoreOut,
+      sort: req.body.sort || {
+        id: -1
+      },
+    }, (err, docsStoreIn) => {
+      if (!err) {
+
+        let whereAccountInvoices = {}
+
+        whereAccountInvoices['company.id'] = site.get_company(req).id
+        whereAccountInvoices['source_type.id'] = 1
+
+        $account_invoices.findMany({
+          select: req.body.select || {},
+          where: whereAccountInvoices,
+          sort: req.body.sort || {
+            id: -1
+          },
+        }, (err, docsAccountInvoices) => {
+          if (!err) {
+
+            if (docsAccountInvoices && docsAccountInvoices.length > 0) {
+              if (docsStoreIn && docsStoreIn.length > 0) {
+
+                docsAccountInvoices.forEach(_d_a_i => {
+                  let foundAi = false
+
+                  docsStoreIn.forEach(_d_s_in => {
+
+                    if (_d_s_in.id === _d_a_i.invoice_id) {
+                      foundAi = true
+                      _d_a_i.total_discount = _d_s_in.total_discount
+                      _d_a_i.total_tax = _d_s_in.total_tax
+                      _d_a_i.current_book_list = _d_s_in.current_book_list
+                      _d_a_i.net_value = _d_s_in.net_value
+                      _d_a_i.total_value_added = _d_s_in.total_value_added
+
+                      if (_d_s_in.currency && _d_s_in.currency.id) {
+                        _d_a_i.currency = _d_s_in.currency
+
+                      }
+
+                      if (_d_s_in.payment_method && _d_s_in.payment_method.id) {
+                        _d_a_i.payment_method = _d_s_in.payment_method
+
+                      }
+
+                      if (_d_s_in.safe && _d_s_in.safe.id) {
+                        _d_a_i.safe = _d_s_in.safe
+
+                      }
+
+                      if (_d_s_in.paid_up) {
+                        _d_a_i.paid_up = _d_s_in.paid_up
+
+                      }
+
+                      $account_invoices.update(_d_a_i);
+                    }
+                  });
+
+                  if (!foundAi) {
+                    $account_invoices.delete(_d_a_i);
+                  }
+
+                });
+              }
+            }
+
+          }
+        })
+
+      }
+      response.done = true
+      res.json(response)
+    })
+  })
 
 
 
