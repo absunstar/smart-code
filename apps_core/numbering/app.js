a38cd58f0660a66824afaa075821fc4c6805076b
AbduAllah Gabbar <you@example.com>
2020-11-27 02:33:35 +0200

    edit

---
+++ apps_core/numbering/app.js
@@ -0,0 +1,112 @@
+module.exports = function init(site) {
+  const $numbering = site.connectCollection("numbering")
+
+  site.get({
+    name: "numbering",
+    path: __dirname + "/site_files/html/index.html",
+    parser: "html",
+    compress: true
+  })
+
+  site.get({
+    name: "/api/modules_list/all",
+    path: __dirname + "/site_files/json/modules_list.json"
+  })
+
+  site.post({
+    name: "/api/type_numbering/all",
+    path: __dirname + "/site_files/json/type_numbering.json"
+  })
+
+  site.get({
+    name: "/images",
+    path: __dirname + "/site_files/images"
+  })
+
+  site.post("/api/numbering/get", (req, res) => {
+
+    let response = {
+      done: false
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    };
+
+    let modules_list = JSON.parse(site.readFileSync(__dirname + '/site_files/json/modules_list.json'))
+
+    if (site.features.like('*erp*')) {
+
+      let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant');
+      modules_list[4].screens_list = screens_list4
+
+    } else if (site.features.like('*restaurant*')) {
+
+      let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
+      modules_list[2].screens_list = screens_list2
+
+    } else if (site.features.like('*pos*')) {
+
+      let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
+      modules_list[2].screens_list = screens_list2
+
+      let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant');
+      modules_list[4].screens_list = screens_list4
+
+    }
+
+    $numbering.get({
+      where: {
+        'company.id': site.get_company(req).id
+      }
+    }, (err, doc, count) => {
+      if (!err && doc) {
+        response.done = true
+        response.doc = doc
+        res.json(response)
+      } else {
+
+        $numbering.add({
+          "modules_list": modules_list,
+          "company": site.get_company(req)
+        }, (err, doc) => {
+          if (!err && doc) {
+            response.done = true
+            response.doc = doc
+            res.json(response)
+          } else {
+            response.error = err.message
+            res.json(response)
+          }
+        })
+      }
+
+    })
+  })
+
+  site.post("/api/numbering/save", (req, res) => {
+    let response = {
+      done: false
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    };
+
+    let data = req.data
+
+
+    $numbering.update(data, (err, result) => {
+      if (!err) {
+        response.done = true
+      } else {
+        response.error = err.message
+      }
+      res.json(response)
+    })
+  })
+}
---
+++ apps_core/numbering/site_files/js/index.js
@@ -0,0 +1,93 @@
+
+let btn1 = document.querySelector('#numbering .tab-link');
+if (btn1) {
+  btn1.click();
+}
+
+app.controller("default_setting", function ($scope, $http) {
+  $scope._search = {};
+
+
+  $scope.setCirculate = function (type) {
+
+    let index = 0;
+
+    if (type === 'main') index = 0;
+    else if (type === 'inventory') index = 1;
+    else if (type === 'accounting') index = 2;
+    else if (type === 'general') index = 3;
+    else if (type === 'employees') index = 4;
+
+
+    $scope.numbering.modules_list[index].screens_list.forEach(_ml => {
+
+      _ml.type_numbering = $scope.numbering.modules_list[index].type_numbering;
+      _ml.first_value = $scope.numbering.modules_list[index].first_value;
+      _ml.last_value = $scope.numbering.modules_list[index].first_value -1;
+      _ml.length_level = $scope.numbering.modules_list[index].length_level;
+    });
+
+  };
+
+  $scope.loadNumbering = function () {
+    $http({
+      method: "POST",
+      url: "/api/numbering/get",
+      data: {}
+    }).then(
+      function (response) {
+
+        if (response.data.done) {
+          $scope.numbering = response.data.doc;
+        } else {
+          $scope.numbering = {};
+        }
+      }
+    )
+  };
+
+
+
+  $scope.typeNumberingList = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $scope.typeNumberingList = [];
+    $http({
+      method: "POST",
+      url: "/api/type_numbering/all"
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        $scope.typeNumberingList = response.data;
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.saveNumbering = function (where) {
+    $scope.busy = true;
+    $http({
+      method: "POST",
+      url: "/api/numbering/save",
+      data: $scope.numbering
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (!response.data.done) {
+          $scope.error = response.data.error
+        }
+
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.loadNumbering();
+  $scope.typeNumberingList();
+});
