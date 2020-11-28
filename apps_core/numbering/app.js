ae5f6f5f8b67d53b6dd8a839766c8e9be58fea62
AbduAllah Gabbar <you@example.com>
2020-11-28 19:15:22 +0200

    edit

--- apps_core/numbering/app.js
+++ apps_core/numbering/app.js
@@ -35,27 +35,6 @@ module.exports = function init(site) {
       return
     };
 
-    let modules_list = JSON.parse(site.readFileSync(__dirname + '/site_files/json/modules_list.json'))
-
-    if (site.features.like('*erp*')) {
-
-      let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant');
-      modules_list[4].screens_list = screens_list4
-
-    } else if (site.features.like('*restaurant*')) {
-
-      let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
-      modules_list[2].screens_list = screens_list2
-
-    } else if (site.features.like('*pos*')) {
-
-      let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
-      modules_list[2].screens_list = screens_list2
-
-      let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant');
-      modules_list[4].screens_list = screens_list4
-
-    }
 
     $numbering.get({
       where: {
@@ -67,6 +46,64 @@ module.exports = function init(site) {
         response.doc = doc
         res.json(response)
       } else {
+
+
+        let modules_list = JSON.parse(site.readFileSync(__dirname + '/site_files/json/modules_list.json'))
+
+        modules_list.forEach(_ml => {
+          _ml.screens_list.forEach(_sl => {
+            _sl.type_numbering = {
+              id: 4,
+              "en": "Manual",
+              "ar": "يدوي"
+          }
+          });
+        });
+
+        if (site.features.like('*erp*')) {
+
+          let screens_list0 = modules_list[0].screens_list.filter(i => i.feature !== 'gym');
+          modules_list[0].screens_list = screens_list0
+          
+          let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant' || i.feature !== 'gym');
+          modules_list[4].screens_list = screens_list4
+
+        } else if (site.features.like('*restaurant*')) {
+
+          let screens_list0 = modules_list[0].screens_list.filter(i => i.feature !== 'gym');
+          modules_list[0].screens_list = screens_list0
+
+          let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
+          modules_list[2].screens_list = screens_list2
+
+          let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'gym');
+          modules_list[4].screens_list = screens_list4
+
+        } else if (site.features.like('*pos*')) {
+
+          let screens_list0 = modules_list[0].screens_list.filter(i => i.feature !== 'gym');
+          modules_list[0].screens_list = screens_list0
+
+          let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
+          modules_list[2].screens_list = screens_list2
+
+          let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant' || i.feature !== 'gym');
+          modules_list[4].screens_list = screens_list4
+
+        } else if (site.features.like('*gym*')) {
+
+          let screens_list0 = modules_list[0].screens_list.filter(i => i.feature !== 'pos');
+          modules_list[0].screens_list = screens_list0
+     
+          let screens_list2 = modules_list[2].screens_list.filter(i => i.feature !== 'erp');
+          modules_list[2].screens_list = screens_list2
+
+          let screens_list4 = modules_list[4].screens_list.filter(i => i.feature !== 'restaurant');
+          modules_list[4].screens_list = screens_list4
+
+
+
+        }
 
         $numbering.add({
           "modules_list": modules_list,
