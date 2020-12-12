663b13be2dd7e042cb8eb9a555d1f933ed3127ac
AbduAllah Gabbar <you@example.com>
2020-12-12 09:25:14 +0200

    edit

---
+++ apps_school/report-attend-students/app.js
@@ -0,0 +1,94 @@
+module.exports = function init(site) {
+  const $attend_students = site.connectCollection("attend_students")
+
+  site.get({
+    name: "report_attend_students",
+    path: __dirname + "/site_files/html/index.html",
+    parser: "html",
+    compress: true
+  })
+
+  site.get({
+    name: 'images',
+    path: __dirname + '/site_files/images/'
+  })
+
+  site.post("/api/report_attend_students/all", (req, res) => {
+    let response = {
+      done: false
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First'
+      res.json(response)
+      return
+    };
+
+    let where = req.data.where || {};
+    let customer = where.customer;
+
+    if (where['code']) {
+      where['code'] = site.get_RegExp(where['code'], 'i')
+    };
+
+    if (where['name']) {
+      where['name'] = site.get_RegExp(where['name'], 'i')
+    };
+
+    if (where.date) {
+      let d1 = site.toDate(where.date)
+      let d2 = site.toDate(where.date)
+      d2.setDate(d2.getDate() + 1)
+      where.date = {
+        '$gte': d1,
+        '$lt': d2
+      }
+    } else if (where && where.date_from) {
+      let d1 = site.toDate(where.date_from)
+      let d2 = site.toDate(where.date_to)
+      d2.setDate(d2.getDate() + 1);
+      where.date = {
+        '$gte': d1,
+        '$lt': d2
+      }
+      delete where.date_from
+      delete where.date_to
+    }
+
+    if (where['customer']) {
+      where['attend_list.customer.id'] = where['customer'].id;
+      delete where['customer']
+      
+    }
+
+    // where['$or'] = [{ 'source_type.id': 2 }, { 'source_type.id': 3 }, { 'source_type.id': 4 }, { 'source_type.id': 7 }]
+
+    where['company.id'] = site.get_company(req).id
+    where['branch.code'] = site.get_branch(req).code
+    $attend_students.findMany({
+      select: req.body.select || {},
+      where: where,
+      sort: req.body.sort || { id: -1 },
+      limit: req.body.limit
+    }, (err, docs, count) => {
+      if (!err && docs) {
+        docs.forEach(_docs => {
+          _docs.attend_list.forEach(_att => {
+            if(customer && customer.id && _att.customer.id == customer.id){
+              _docs.status = _att.status
+              _docs.attend_time = _att.attend_time
+              _docs.leave_time = _att.leave_time
+            }
+          });
+        });
+        response.done = true
+        response.list = docs
+        response.count = count
+      } else {
+        response.error = err.message
+      }
+      res.json(response)
+    })
+  })
+
+}
---
+++ apps_school/report-attend-students/site_files/js/index.js
@@ -0,0 +1,115 @@
+app.controller("report_attend_students", function ($scope, $http, $timeout) {
+  $scope._search = {};
+
+  $scope.report_attend_students = {};
+
+  $scope.getCustomersList = function (ev) {
+    $scope.busy = true;
+    if (ev.which === 13) {
+      $http({
+        method: "POST",
+        url: "/api/customers/all",
+        data: {
+          search: $scope.search_customer,
+          where: {
+            active: true
+          }
+        }
+      }).then(
+        function (response) {
+          $scope.busy = false;
+          if (response.data.done && response.data.list.length > 0) {
+            $scope.studentsList = response.data.list;
+          }
+        },
+        function (err) {
+          $scope.busy = false;
+          $scope.error = err;
+        }
+      )
+    }
+  };
+
+  $scope.getReportAttendstudentsList = function (where) {
+    $scope.busy = true;
+    $scope.list = [];
+    $scope.count = 0;
+    $http({
+      method: "POST",
+      url: "/api/report_attend_students/all",
+      data: {
+        where: where
+      }
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done && response.data.list.length > 0) {
+          $scope.list = response.data.list;
+          $scope.attend_count = 0;
+          $scope.absence_count = 0;
+        }
+        if ($scope.customer && $scope.customer.id) {
+
+          $scope.list.forEach(_list => {
+            if (_list.status && _list.status.name == 'attend') {
+              $scope.attend_count = $scope.attend_count + 1
+
+            } else if (_list.status && _list.status.name == 'absence') {
+              $scope.absence_count = $scope.absence_count + 1
+            }
+          });
+        } else {
+          $scope.list.forEach(_list => {
+            _list.attend_list.forEach(_att => {
+
+              if (_att.status && _att.status.name == 'attend') {
+                $scope.attend_count = $scope.attend_count + 1
+
+              } else if (_att.status && _att.status.name == 'absence') {
+                $scope.absence_count = $scope.absence_count + 1
+              }
+
+            });
+
+
+          });
+        }
+
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    )
+  };
+
+  $scope.viewStudentsAttend = function (c) {
+
+    $scope.attend = c;
+    $scope.attend.attend_count = 0;
+    $scope.attend.absence_count = 0;
+    $scope.attend.attend_list.forEach(_att => {
+      if (_att.status.name == 'attend') {
+        $scope.attend.attend_count = $scope.attend.attend_count + 1
+
+      } else if (_att.status.name == 'absence') {
+        $scope.attend.absence_count = $scope.attend.absence_count + 1
+      }
+    });
+    site.showModal('#reportAttendDetailsModal');
+
+  };
+
+  $scope.searchAll = function () {
+    $scope._search = {};
+
+    if ($scope.search)
+      $scope.customer = $scope.search.customer;
+
+    $scope.getReportAttendstudentsList($scope.search);
+    site.hideModal('#reportAttendCustomersSearchModal');
+    $scope.search = {}
+  };
+
+  $scope.getReportAttendstudentsList({ date: new Date() });
+});
