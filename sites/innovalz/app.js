433a0485fadb9bdd872402812201edf39cb702a2
ahmed2122 <50507120+3bnaser12@users.noreply.github.com>
2021-09-15 15:24:37 +0200

    naser2

--- sites/innovalz/app.js
+++ sites/innovalz/app.js
@@ -6,6 +6,26 @@ module.exports = function init(site) {
   })
 
   site.get({
+    name: 'css',
+    path: __dirname + '/site_files/css/'
+  })
+
+  site.get({
+    name: 'js',
+    path: __dirname + '/site_files/js/'
+  })
+
+  site.get({
+    name: 'webfonts',
+    path: __dirname + '/site_files/webfonts/'
+  })
+
+  site.get({
+    name: 'fonts',
+    path: __dirname + '/site_files/fonts/'
+  })
+
+  site.get({
     name: "/",
     path: __dirname + "/site_files/html/index.html",
     parser: "html",
