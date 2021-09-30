e44dcf283feaf07ead6beee33c6a77e6ebac6137
ahmed2122 <50507120+3bnaser12@users.noreply.github.com>
2021-09-30 17:11:30 +0200

    route

--- apps_innovalz/in-medic/app.js
+++ apps_innovalz/in-medic/app.js
@@ -10,4 +10,35 @@ module.exports = function init(site) {
     parser: 'html',
     compress: true,
   });
+  site.get({
+    name: 'booking',
+    path: __dirname + '/site_files/html/book.html',
+    parser: 'html',
+    compress: true,
+  });
+  site.get({
+    name: 'profile',
+    path: __dirname + '/site_files/html/profile.html',
+    parser: 'html',
+    compress: true,
+  });
+  site.get({
+    name: 'scan',
+    path: __dirname + '/site_files/html/scan.html',
+    parser: 'html',
+    compress: true,
+  });
+  site.get({
+    name: 'analysis',
+    path: __dirname + '/site_files/html/analysis.html',
+    parser: 'html',
+    compress: true,
+  });
+  site.get({
+    name: 'authentication',
+    path: __dirname + '/site_files/html/creditional.html',
+    parser: 'html',
+    compress: true,
+  });
+  
 };
