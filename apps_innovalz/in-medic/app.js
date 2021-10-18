7643c3ce24c4ff7e11c800e776657d24ab0468a2
ahmed2122 <50507120+3bnaser12@users.noreply.github.com>
2021-10-18 19:04:10 +0200

    abdallah-15-10-2021

--- apps_innovalz/in-medic/app.js
+++ apps_innovalz/in-medic/app.js
@@ -16,6 +16,13 @@ module.exports = function init(site) {
     parser: 'html',
     compress: true,
   });
+
+  site.get({
+    name: 'vaccine',
+    path: __dirname + '/site_files/html/vaccine.html',
+    parser: 'html',
+    compress: true,
+  });
   site.get({
     name: 'profile',
     path: __dirname + '/site_files/html/profile.html',
