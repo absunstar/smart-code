f4a5434cf772ec30da41f492ad4d85142c84bb2e
absunstar <absunstar@gmail.com>
2022-03-15 23:58:43 +0200

    #15-3-2022

--- apps_inventories/barcode-printer/site_files/js/index.js
+++ apps_inventories/barcode-printer/site_files/js/index.js
@@ -110,7 +110,7 @@ app.controller('barcode_printer', function ($scope, $http, $timeout) {
                 $timeout(() => {
                     site.print(
                         {
-                            content: document.querySelector('#barcodePrint').innerHTML,
+                            content: document.querySelector('#barcodePage').innerHTML,
                             selector: '#barcodePrint',
                             ip: '127.0.0.1',
                             port: '60080',
