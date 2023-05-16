4b992d42380e95539b3e294919c3657e35b6387b
absunstar <absunstar@gmail.com>
2023-05-16 17:25:18 +0300

    #15-3-2022

--- cms.js
+++ cms.js
@@ -84,9 +84,9 @@ site.get(
         'theme1/article.html',
         {
           site_name: site.setting.languagesList[0].siteName,
-          page_logo: doc.imageURL || site.setting.siteLogo.url,
-          page_title: site.setting.languagesList[0].siteName + site.setting.languagesList[0].titleSeparator + doc.title,
-          page_description: doc.description,
+          page_logo: article.imageURL || site.setting.siteLogo.url,
+          page_title: site.setting.languagesList[0].siteName + site.setting.languagesList[0].titleSeparator + article.title,
+          page_description: article.description,
           prayerTimingsList: site.setting.prayerTimingsList,
           categoriesDisplayList1: site.categoriesDisplayList1,
           categoriesDisplayList2: site.categoriesDisplayList2,
