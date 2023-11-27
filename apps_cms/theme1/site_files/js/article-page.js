5f43dae09443e9f3ff2801c9cd00a6cf6bdd6277
absunstar <absunstar@gmail.com>
2023-11-27 14:46:01 +0200

    #25-11-2023

--- apps_cms/theme1/site_files/js/article-page.js
+++ apps_cms/theme1/site_files/js/article-page.js
@@ -3,18 +3,3 @@ function playCurrentVideo() {
     videoPlayer.play();
   }
 }
-/*
-let allcloseAButton = document.querySelectorAll('.closeAButton');
-let allaudiopalyer = document.querySelectorAll('.audiopalyer');
-let allaudioFile = document.querySelectorAll('.audioFile');
-for (let i = 0; i < allaudioFile.length; i++) {
-  allaudioFile[i].addEventListener('mousedown', () => {
-    allaudiopalyer[i].classList.add('showAnddisplay');
-  });
-  for (let i = 0; i < allcloseAButton.length; i++) {
-    allcloseAButton[i].addEventListener('mousedown', () => {
-      allaudiopalyer[i].classList.remove('showAnddisplay');
-    });
-  }
-}
-*/
