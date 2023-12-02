52e6d450a4acb66df5d232b3de8c85df7c25fda8
absunstar <absunstar@gmail.com>
2023-12-03 00:02:37 +0200

    #25-11-2023

--- apps_cms/articles/app.js
+++ apps_cms/articles/app.js
@@ -1040,12 +1040,17 @@ module.exports = function init(site) {
           <link>${$url}</link>
           <image>${domain}/article-image/${doc.guid}</image>
           <description>${doc.$description}</description>
+          <content:encoded><![CDATA[
+            ${doc.$description}
+          ]]></content:encoded>
           <pubDate>${$date}</pubDate>
         </item>
         `;
     });
     let xml = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" 
+    <rss version="2.0" 
+    xmlns:atom="http://www.w3.org/2005/Atom" 
+    xmlns:image="http://purl.org/rss/1.0/modules/image/"
     xmlns:content="http://purl.org/rss/1.0/modules/content/">
       <channel>
             <title> ${lang.siteName} ${text} Global RSS</title>
