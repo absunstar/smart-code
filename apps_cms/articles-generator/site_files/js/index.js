0496317bb684150ec5207463cf7980f23dc5dbb4
absunstar <absunstar@gmail.com>
2023-12-09 18:01:35 +0200

    #25-11-2023

--- apps_cms/articles-generator/site_files/js/index.js
+++ apps_cms/articles-generator/site_files/js/index.js
@@ -67,7 +67,7 @@ app.connectScope(
         url: `/api/generator/get-blogger-posts`,
         method: 'POST',
       }).then((res) => {
-        $scope.bloggerPostList = res.data.list || [];
+        $scope.bloggerPostList = res.data.list;
       });
     };
 
