a19a1a5a3b45e7d3a75b7cd4e311a0b7e61de55f
absunstar <absunstar@gmail.com>
2024-05-29 15:50:13 +0300

    #25-11-2023

--- appsLawyer/words/app.js
+++ appsLawyer/words/app.js
@@ -1,24 +1,24 @@
 module.exports = function init(site) {
-  const $words = site.connectCollection("words");
+  const $words = site.connectCollection('words');
 
   site.get(
     {
-      name: "words",
+      name: 'words',
     },
     (req, res) => {
       res.render(
-        "words" + "/index.html",
+        'words' + '/index.html',
         {
-          title: "words",
-          appName: "Words",
+          title: 'words',
+          appName: 'Words',
           setting: site.getSiteSetting(req.host),
         },
-        { parser: "html", compres: true }
+        { parser: 'html', compres: true }
       );
     }
   );
 
-  site.post({ name: "/x-api/words/importNewWords" }, (req, res) => {
+  site.post({ name: '/api/words/importNewOnly' }, (req, res) => {
     let response = {
       done: false,
       file: req.form.files.fileToUpload,
@@ -26,15 +26,11 @@ module.exports = function init(site) {
 
     if (site.isFileExistsSync(response.file.filepath)) {
       let docs = [];
-      if (response.file.originalFilename.like("*.xls*")) {
+      if (response.file.originalFilename.like('*.xls*')) {
         let workbook = site.XLSX.readFile(response.file.filepath);
-        docs = site.XLSX.utils.sheet_to_json(
-          workbook.Sheets[workbook.SheetNames[0]]
-        );
+        docs = site.XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
       } else {
-        docs = site.fromJson(
-          site.readFileSync(response.file.filepath).toString()
-        );
+        docs = site.fromJson(site.readFileSync(response.file.filepath).toString());
       }
 
       if (Array.isArray(docs)) {
@@ -46,68 +42,43 @@ module.exports = function init(site) {
 
         site.words.save();
       } else {
-        site.dbMessage = "can not import unknown type : " + site.typeof(docs);
-        console.log(site.dbMessage);
+        response.error = 'can not import unknown type : ' + site.typeof(docs);
       }
     } else {
-      site.dbMessage = "file not exists : " + response.file.filepath;
-      console.log(site.dbMessage);
+      response.error = 'file not exists : ' + response.file.filepath;
     }
+    res.json(response);
   });
-
-  site.post({ name: "/x-api/words/importReplaceWords" }, (req, res) => {
+  site.post({ name: '/api/words/importAndReplace' }, (req, res) => {
     let response = {
       done: false,
       file: req.form.files.fileToUpload,
     };
+
     if (site.isFileExistsSync(response.file.filepath)) {
-      let wordsList = [];
-      if (response.file.originalFilename.like("*.xls*")) {
+      let docs = [];
+      if (response.file.originalFilename.like('*.xls*')) {
         let workbook = site.XLSX.readFile(response.file.filepath);
-        wordsList = site.XLSX.utils.sheet_to_json(
-          workbook.Sheets[workbook.SheetNames[0]]
-        );
+        docs = site.XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
       } else {
-        wordsList = site.fromJson(
-          site.readFileSync(response.file.filepath).toString()
-        );
+        docs = site.fromJson(site.readFileSync(response.file.filepath).toString());
       }
-      if (Array.isArray(wordsList)) {
-        $words.findMany({}, (err, docs) => {
-          let num = 0;
-          let num2 = 0;
-          for (let i = 0; i < wordsList.length; i++) {
-            if (wordsList[i].Ar || wordsList[i].En) {
-              let index = docs.findIndex(
-                (w) =>
-                  w.name == wordsList[i].name ||
-                  w.Ar == wordsList[i].Ar ||
-                  w.En == wordsList[i].En
-              );
-              if (index != -1) {
-                num += 1;
-                delete wordsList[i].id;
-                delete wordsList[i]._id;
-                site.words.set(wordsList[i]);
-              } else {
-                num2 += 1;
-                delete wordsList[i].id;
-                delete wordsList[i]._id;
-                site.words.add(wordsList[i]);
-              }
-            }
-          }
-          site.words.save();
-        });
+
+      if (Array.isArray(docs)) {
+        for (let i = 0; i < docs.length; i++) {
+          delete docs[i].id;
+          delete docs[i]._id;
+          site.words.set(docs[i]);
+        }
+
+        site.words.save();
       } else {
-        site.dbMessage =
-          "can not import unknown type : " + site.typeof(wordsList);
-        console.log(site.dbMessage);
+        response.error = 'can not import unknown type : ' + site.typeof(docs);
       }
     } else {
-      site.dbMessage = "file not exists : " + response.file.filepath;
-      console.log(site.dbMessage);
+      response.error = 'file not exists : ' + response.file.filepath;
     }
+    res.json(response);
   });
 
   site.post({ name: `/x-api/words/export`, public: true }, (req, res) => {
@@ -118,9 +89,9 @@ module.exports = function init(site) {
     $words.findMany({}, (err, wordsList) => {
       if (!err && wordsList) {
         response.done = true;
-        site.fs.writeFileSync("wordsFileLawyer.json", JSON.stringify(wordsList));
+        site.fs.writeFileSync('wordsFileLawyer.json', JSON.stringify(wordsList));
       } else {
-        response.error = err?.message || "Not Exists";
+        response.error = err?.message || 'Not Exists';
       }
       res.json(response);
     });
