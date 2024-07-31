323ed77f735df7dfe77406b38d4abb9f86f50e46
unknown <AbduAllahGabbar>
2024-07-31 11:50:42 +0300

    #01-06-2024

--- appsTeacher/centers/site_files/js/centersView.js
+++ appsTeacher/centers/site_files/js/centersView.js
@@ -2,56 +2,53 @@ app.controller("centersView", function ($scope, $http, $timeout) {
   $scope.list = [];
   $scope.baseURL = "";
 
-  $scope.getAll = function (ev) {
+  $scope.getAll = function () {
     $scope.busy = true;
     $scope.error = "";
-    if (ev.which === 13) {
-      $http({
-        method: "POST",
-        url: `${$scope.baseURL}/api/centers/all`,
-        data: {
-          view : true,
-          select: {
-            id: 1,
-            host: 1,
-            name: 1,
-            image: 1,
-            daysList: 1,
-            mobile: 1,
-            educationalLevel: 1,
-            schoolYear: 1,
-            country: 1,
-            gov: 1,
-            city: 1,
-            area: 1,
-            address: 1,
-            latitude: 1,
-            longitude: 1,
-          },
+    $http({
+      method: "POST",
+      url: `${$scope.baseURL}/api/centers/all`,
+      data: {
+        view: true,
+        select: {
+          id: 1,
+          host: 1,
+          name: 1,
+          image: 1,
+          daysList: 1,
+          mobile: 1,
+          educationalLevel: 1,
+          schoolYear: 1,
+          country: 1,
+          gov: 1,
+          city: 1,
+          area: 1,
+          address: 1,
+          latitude: 1,
+          longitude: 1,
         },
-      }).then(
-        function (response) {
-          $scope.busy = false;
-          if (response.data.done) {
-            $scope.list = response.data.list;
-            setTimeout(() => {
-              $scope.list.forEach(_item => {
-                _item.locationSrc =`https://maps.google.com/maps?q=${_item.latitude},${_item.longitude}&hl=es;z=14&output=embed`;
-            
-                  $scope.$applyAsync();
-           
-                });
-            }, 1000);
-          } else {
-            $scope.error = response.data.error;
-          }
-        },
-        function (err) {
-          console.log(err);
+      },
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.list = response.data.list;
+          setTimeout(() => {
+            $scope.list.forEach((_item) => {
+              _item.locationSrc = `https://maps.google.com/maps?q=${_item.latitude},${_item.longitude}&hl=es;z=14&output=embed`;
+
+              $scope.$applyAsync();
+            });
+          }, 1000);
+        } else {
+          $scope.error = response.data.error;
         }
-      );
-    }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
   };
 
-  $scope.getAll({ which: 13 });
+  $scope.getAll();
 });
