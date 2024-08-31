452254a25212aaf556ad90f96e115cb3ce13a89d
unknown <AbduAllahGabbar>
2024-08-31 19:45:34 +0300

    #05-08-2024

--- appsTeacher/groups/app.js
+++ appsTeacher/groups/app.js
@@ -325,6 +325,7 @@ module.exports = function init(site) {
             let index = doc.dayList.findIndex(
               (itm) => new Date(itm.date).getDate() === date.getDate() && new Date(itm.date).getMonth() === date.getMonth() && new Date(itm.date).getFullYear() === date.getFullYear() && !itm.isBook
             );
+            console.log(doc.dayList);
             
             if (index !== -1) {
               if (!doc.dayList[index].isBook) {
