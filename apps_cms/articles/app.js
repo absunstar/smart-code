f6ca5b1106db16f42a6342fcc5d340c316710f6b
absunstar <absunstar@gmail.com>
2025-01-03 22:46:55 +0200

    #05-08-2024

--- apps_cms/articles/app.js
+++ apps_cms/articles/app.js
@@ -1639,7 +1639,7 @@ module.exports = function init(site) {
         let xml = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           <url>
-            <loc>${domain} - ${limit}</loc>
+            <loc>${domain}</loc>
             <lastmod>${new Date().toISOString()}</lastmod>
             <changefreq>always</changefreq>
             <priority>1</priority>
