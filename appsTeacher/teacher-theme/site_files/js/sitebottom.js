c8b4dab3675ace22768770c753a402eb7f953e3c
unknown <AbduAllahGabbar>
2025-02-07 16:40:15 +0200

    #05-08-2024

--- appsTeacher/teacher-theme/site_files/js/sitebottom.js
+++ appsTeacher/teacher-theme/site_files/js/sitebottom.js
@@ -1,34 +1,30 @@
-var app = app || angular.module('myApp', []);
+var app = app || angular.module("myApp", []);
 
+app.controller("sitebottom", ($scope, $http) => {
+  $scope.register = function () {
+    site.showModal("#registerModal");
+  };
 
+  $scope.login = function () {
+    site.showModal("#loginModal");
+  };
 
-app.controller('sitebottom' , ($scope , $http)=>{
-
-    $scope.register = function(){
-        site.showModal('#registerModal');
-    };
-
-    $scope.login = function(){
-        site.showModal('#loginModal');
-    };
-
-    $scope.changeLang = function (language) {
-        if (typeof language == 'string') {
-          language = { id: language, dir: 'rtl', text: 'right' };
-          if (language.id.like('*en*')) {
-            language.dir = 'ltr';
-            language.text = 'left';
-          }
-        }
-        $http({
-          method: 'POST',
-          url: '/x-language/change',
-          data: language,
-        }).then(function (response) {
-          if (response.data.done) {
-            window.location.reload(!0);
-          }
-        });
-      };
-
+  $scope.changeLang = function (language) {
+    if (typeof language == "string") {
+      language = { id: language, dir: "rtl", text: "right" };
+      if (!language.id.like("*ar*")) {
+        language.dir = "ltr";
+        language.text = "left";
+      }
+    }
+    $http({
+      method: "POST",
+      url: "/x-language/change",
+      data: language,
+    }).then(function (response) {
+      if (response.data.done) {
+        window.location.reload(!0);
+      }
+    });
+  };
 });
