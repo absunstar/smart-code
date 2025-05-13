5f2fcb7d31921c8b0b4433cc6515311501b7504c
absunstar <absunstar@gmail.com>
2025-05-13 22:51:01 +0300

    #05-08-2024

--- apps_cms/articles-generator/site_files/js/get-youtube-video-info.js
+++ apps_cms/articles-generator/site_files/js/get-youtube-video-info.js
@@ -1,14 +1,16 @@
-SOCIALBROWSER.onLoad(() => {
-  alert('Youtube Info Activated');
-  let channelLogoSelector = 'yt-decorated-avatar-view-model img[src]';
-  function sendData() {
-    let title = document.title;
-    SOCIALBROWSER.share({ type: 'generator-youtube-video-info', title: title, url: document.location.href, image: { url: document.querySelector(channelLogoSelector).src } });
-    SOCIALBROWSER.currentWindow.close();
-  }
-  setInterval(() => {
-    if (document.querySelector(channelLogoSelector)) {
-      sendData();
-    }
-  }, 1000 * 3);
-});
+module.exports = function (SOCIALBROWSER, window, document) {
+    SOCIALBROWSER.onLoad(() => {
+        alert('Youtube Info Activated');
+        let channelLogoSelector = 'yt-decorated-avatar-view-model img[src]';
+        function sendData() {
+            let title = document.title;
+            SOCIALBROWSER.share({ type: 'generator-youtube-video-info', title: title, url: document.location.href, image: { url: document.querySelector(channelLogoSelector).src } });
+            SOCIALBROWSER.currentWindow.close();
+        }
+        setInterval(() => {
+            if (document.querySelector(channelLogoSelector)) {
+                sendData();
+            }
+        }, 1000 * 3);
+    });
+};
